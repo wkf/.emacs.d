88925583dd1ff61dce5f251e97c825781f703dc8
Will Farrell <wkfarrell@gmail.com>
2020-12-26 22:29:12 -0500

    Rename unused function in user-util, for some reason.

--- user/user-util.el
+++ user/user-util.el
@@ -44,8 +44,8 @@
     (projectile-switch-project-by-name directory)
     (setq projectile-switch-project-action default-action)))
 
-(defun user/switch-to-last-buffer ()
-  "Switch to last buffer in current window."
+(defun user/switch-to-other-buffer ()
+  "Switch to other buffer in current window."
   (interactive)
   (switch-to-buffer nil))
 
